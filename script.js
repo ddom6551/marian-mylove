f1022d85d0ae2c81dec693e802735ead9da7aafe
ddom6551 <128395304+ddom6551@users.noreply.github.com>
2025-05-15 21:52:14 +0800

    script.js

--- script.js
+++ script.js
@@ -1,19 +1,19 @@
-const moods = ["Devastated","Sad","Neutral","Happy","Glorious"];
 const form = document.getElementById('grievance-form');
 const formBox = document.getElementById("form-box");
 
 form.addEventListener('submit', e => {
   e.preventDefault();
+  const other = form.response.value === "other" ? form.otherResponse.value : "";
   const data = {
     title: document.getElementById('title').value,
     description: document.getElementById('description').value,
     mood: form.mood.value,
     severity: form.severity.value,
+    response: form.response.value === "other" ? other : form.response.value,
     timestamp: new Date().toLocaleString()
   };
   fetch('https://script.google.com/macros/s/AKfycbzCScHdC9A89s6sbf5en8SCjfspdM9mlnNWMn9_LjqkgJqPGoOgYEk1fomBJsZebKxjVg/exec', {
     method: 'POST',
-    mode: 'no-cors',
     headers: { 'Content-Type': 'application/json' },
     body: JSON.stringify(data)
   }).then(() => {
