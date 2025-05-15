0b4094bbb5d5c33b3c6486044c8df748fca34e02
ddom6551 <128395304+ddom6551@users.noreply.github.com>
2025-05-15 21:21:38 +0800

    script.js

--- script.js
+++ script.js
@@ -20,9 +20,13 @@ form.addEventListener('submit', e => {
     document.body.classList.add("thankyou-mode");
     formBox.innerHTML = `
       <div id="thank-you-message">
+        <div class="heart-sparkle"></div>
+        <div class="heart-sparkle" style="top: 50px; left: 80%;"></div>
+        <div class="heart-sparkle" style="top: 80%; left: 40%;"></div>
         <h2>💖 Thank you for sharing, my love.</h2>
         <p>I promise to listen, understand, and grow with you.</p>
       </div>
     `;
+    document.getElementById("thank-you-message").style.display = 'block';
   });
 });
